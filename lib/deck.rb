ee705a168a11b1f0f43a6c6d1f24478922c77dc4
Galen <galen.h.white@gmail.com>
2018-03-13 13:15:53 -0400

    Created Deck class and tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,16 @@
+SUITS = ["Hearts", "Diamonds", "Spades", "Clubs"]
+VALUES = [2,3,4,5,6,7,8,9,10, "A", "K", "Q", "J"]
+
 class Deck
   # Your code here
+  attr_reader :deck
+  def initialize
+    deck = []
+    SUITS.each do |suit|
+      VALUES.each do |value|
+        deck << Card.new(suit, value)
+      end
+    end
+    @deck = deck
+  end
 end
