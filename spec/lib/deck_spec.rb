135970eed22cd56b1026d67e20a0d1a9e085d860
Galen <galen.h.white@gmail.com>
2018-03-13 13:10:07 -0400

    Created Card class and Tests

--- spec/lib/deck_spec.rb
+++ spec/lib/deck_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 RSpec.describe Deck do
   # Your tests here
   it "contains 52 cards" do
-    
+    test_deck = Deck.new
+    expect(test_deck.deck.length).to eq(52)
   end
 end
