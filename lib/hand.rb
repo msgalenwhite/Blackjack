31fe7adadf7ad93bb70911b4e8cc2d23f882af8f
Galen <galen.h.white@gmail.com>
2018-03-13 13:43:54 -0400

    Wrote Hand class and tests to total hand

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,3 +1,33 @@
 class Hand
   # Your code here
+  attr_accessor :hand
+
+  def initialize
+    @hand = []
+  end
+
+  def calculate_total
+    total = 0
+    @hand.each do |card|
+      if card.value.to_f === 0.0
+        if card.value != "A"
+          total += 10
+        else
+          total += add_an_ace(total)
+        end
+      else
+        total += card.value
+      end
+    end
+    return total
+  end
+
+  def add_an_ace(total)
+    if total + 10 > 21
+      return 1
+    end
+
+    return 11
+  end
+
 end
--- spec/lib/hand_spec.rb
+++ spec/lib/hand_spec.rb
@@ -2,10 +2,45 @@ require "spec_helper"
 
 RSpec.describe Hand do
   # Your tests here
-  it "holds cards a player was dealt" do
-
+  before(:each) do
+    @test_hand = Hand.new
   end
-  it "calculates a hand total" do
-    
+
+  it "holds cards a player was dealt" do
+    expect {@test_hand.hand}.to_not raise_error
+  end
+  it "can be given a card to hold" do
+    @test_hand.hand << Card.new("Hearts", 10)
+    expect(@test_hand.hand.length).to eq(1)
+  end
+
+  describe "#calculate_total" do
+    before(:all) do
+      @test_hand = Hand.new
+    end
+
+    it "can sum number cards" do
+      @test_hand.hand << Card.new("Hearts", 10)
+      expect(@test_hand.calculate_total).to eq(10)
+    end
+
+    it "can add a non-ace face-method" do
+      @test_hand.hand << Card.new("Hearts", 10)
+      @test_hand.hand << Card.new("Hearts", "K")
+      expect(@test_hand.calculate_total).to eq(20)
+    end
+
+    it "can add an ace as a 1" do
+      @test_hand.hand << Card.new("Hearts", 10)
+      @test_hand.hand << Card.new("Hearts", "K")
+      @test_hand.hand << Card.new("Hearts", "A")
+      expect(@test_hand.calculate_total).to eq(21)
+    end
+
+    it "can add an ace as an 11" do
+      @test_hand.hand << Card.new("Hearts", 10)
+      @test_hand.hand << Card.new("Hearts", "A")
+      expect(@test_hand.calculate_total).to eq(21)
+    end
   end
 end
