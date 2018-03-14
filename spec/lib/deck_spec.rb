7341e07545e0a60a3b8de772f94d50a1c47c6dcb
Galen <galen.h.white@gmail.com>
2018-03-14 11:27:07 -0400

    Wrote test to make sure deck was shuffled

--- spec/lib/deck_spec.rb
+++ spec/lib/deck_spec.rb
@@ -3,9 +3,15 @@ require "spec_helper"
 RSpec.describe Deck do
   # Your tests here
   describe "#create_deck" do
+    let (:test_deck) {Deck.new}
+    let (:test_deck_two) {Deck.new}
+
     it "contains 52 cards" do
-      test_deck = Deck.new
       expect(test_deck.deck.length).to eq(52)
+    end
+
+    it "shuffles the deck" do
+      expect(test_deck.deck).not_to eq(test_deck_two.deck)
     end
   end
 end
