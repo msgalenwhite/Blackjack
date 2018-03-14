403eb375e51ded85edfb86de4dc6fe74f2809f75
Galen <galen.h.white@gmail.com>
2018-03-14 12:55:58 -0400

    Launch Academy asked that deal_cards output a string for each card

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -2,11 +2,23 @@ require "spec_helper"
 
 RSpec.describe Game do
   let (:test_game) {Game.new("Sally")}
+  let (:test_game_two) {Game.new}
   let (:dealer_hand) {[Card.new("Hearts", 5), Card.new("Diamonds", 10)]}
 
   describe "#initialize" do
     it "is a Game object" do
       expect(test_game).to be_a(Game)
+    end
+    it "has a deck" do
+      expect(test_game.game_deck).to be_a(Array)
+    end
+    context "stores a player's name" do
+      it "can store a custom name" do
+        expect(test_game.player_name).to eq("Sally")
+      end
+      it "can use a default name" do
+        expect(test_game_two.player_name).to eq("Player")
+      end
     end
     it "has a player hand" do
       expect(test_game.player).to be_a(Hand)
@@ -24,7 +36,6 @@ RSpec.describe Game do
         expect(test_game.game_deck.length).to eq(51)
       end
     end
-
     context "with 1 argument" do
       it "takes in a number and adds that many cards to player's hand" do
         test_game.deal_cards(5)
@@ -32,7 +43,6 @@ RSpec.describe Game do
         expect(test_game.game_deck.length).to eq(47)
       end
     end
-
     context "with 2 arguments" do
       it "takes in a number, and adds that number of cards to the dealer's hand" do
         test_game.deal_cards(5, "dealer")
@@ -40,9 +50,14 @@ RSpec.describe Game do
         expect(test_game.game_deck.length).to eq(47)
       end
     end
+  end
 
-    it "prints a message to console with what the player was dealt" do
-
+  describe "#print_cards" do
+    it "takes in an array of card objects" do
+      expect{ test_game.print_cards(dealer_hand, "Dealer") }.not_to raise_error
+    end
+    it "outputs a string of who got what cards" do
+      expect { test_game.print_cards(dealer_hand, "Dealer") }.to output("Dealer was dealt the 5 of Hearts\nDealer was dealt the 10 of Diamonds\n").to_stdout
     end
   end
 
